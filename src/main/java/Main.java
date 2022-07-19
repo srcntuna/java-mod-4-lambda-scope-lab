0974c6885e0aa3275abfe3fc5d6e104a4f61c990
Sercan Tuna <ssercantuna@hotmail.com>
2022-07-19 13:46:14 -0400

    done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,25 @@
 import java.util.function.UnaryOperator;
 
 public class Main {
-    String PREFIX = "__prefix__";
-    String SUFFIX = "__suffix__";
+    final static String PREFIX = "__prefix__";
+    final static String SUFFIX = "__suffix__";
 	
 	// fix the operator code
-    public static UnaryOperator<String> operator = str -> str;
+    public static UnaryOperator<String> operator = str -> {
+        String trimmedStr = str.trim();
+
+        return PREFIX+trimmedStr+SUFFIX;
+
+    };
 
     public static void main(String[] args) {
         String res = operator.apply("  hello"); 
         System.out.println(res); // __prefix__hello__suffix__
+    }
+
+    interface UnaryOperator<String>{
+
+        String apply(String str);
+
     }
 }
